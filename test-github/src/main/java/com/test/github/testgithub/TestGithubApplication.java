46ce52cfee16f7f96a76c6f93827325743a137e3
test-github <1183676409@qq.com>
2019-07-31 21:19:27 +0800

    测试github账户

---
+++ test-github/src/main/java/com/test/github/testgithub/TestGithubApplication.java
@@ -0,0 +1,13 @@
+package com.test.github.testgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestGithubApplication.class, args);
+  }
+
+}
